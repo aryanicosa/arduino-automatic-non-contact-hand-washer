4daf915c4e7bfbcb8d9ddfa267093c7e19907489
JE TECH <69202854+aryanicosa@users.noreply.github.com>
2021-01-08 01:53:00 +0700

    Add files via upload

---
+++ Project Non Contact Hand Washer_Arduino/DesainProgramBaruHandWasher/DesainProgramBaruHandWasher.ino
@@ -0,0 +1,40 @@
+int airPin = 5;
+int sabunPin = 8;
+int blowerPin = 7;
+int ultVal = 0;
+
+int LEDair = 11;
+int LEDsabun = 12;
+int LEDblower = 13;
+
+int pirState = LOW;
+#define trigPin 3
+#define echoPin 4
+#define led 10
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(airPin, OUTPUT);
+  pinMode(sabunPin, OUTPUT);
+  pinMode(blowerPin, OUTPUT);
+  pinMode(LEDair, OUTPUT);
+  pinMode(LEDsabun, OUTPUT);
+  pinMode(LEDblower, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  }
